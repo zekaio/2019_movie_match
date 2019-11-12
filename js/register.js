07bcb9dd2330f9a4ab9093c3187bdf85412f2553
zekaio <zekailing@gmail.com>
2019-11-12 19:31:39 +0800

    美化单选框存档

---
+++ js/register.js
@@ -0,0 +1,10 @@
+var windowHeight = window.innerHeight;
+console.log(windowHeight);
+document.body.style.height = windowHeight+"px";
+// document.getElementById  
+// console.log(document.getElementById('test').offsetTop);
+function submit(){
+    let nickname = document.getElementById('nickname').value;
+    let grade = document.getElementById('grade').value;
+
+}   
