8eb3598ee6021ffbb59f8a056fe257aeb51aae71
zekaio <zekailing@gmail.com>
2019-11-13 15:25:16 +0800

    不管我们遇到什么困难都不要怕

--- js/canvasInit.js
+++ js/canvasInit.js
@@ -2,6 +2,10 @@ var windowHeight = window.innerHeight;
 var windowWidth = window.innerWidth;
 var canvas, ctx, padHeight, padTop;
 var padLeft = 0.1921 * windowWidth;
+let painting = false;
+let draw = false;
+let lastPoint = { x: undefined, y: undefined };
+
 function canvasInit() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
@@ -14,4 +18,55 @@ function canvasInit() {
   canvas.style.top = padTop + padHeight * 0.088058 + "px";
   ctx.strokeStyle = "#c4b46b";
   ctx.beginPath();
+  if (document.body.ontouchstart !== undefined) {
+    // 使用touch事件
+    canvas.ontouchstart = function (e) {
+      // 开始触摸
+      if (!draw) {
+        e.preventDefault();
+        document.getElementById('error').innerText = "";
+        let scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        let x = e.touches[0].clientX - padLeft;
+        let y = e.touches[0].clientY - (padTop - scrollTop + padHeight * 0.088058);
+        painting = true;
+        lastPoint = { x, y };
+        ctx.save();
+        drawCircle(x, y, 0);
+      }
+    }
+    canvas.ontouchmove = function (e) {
+      // 开始滑动
+      if (painting) {
+        e.preventDefault();
+        let scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        let x = e.touches[0].clientX - padLeft;
+        let y = e.touches[0].clientY - (padTop - scrollTop + padHeight * 0.088058);
+        let newPoint = { x, y };
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+        lastPoint = newPoint;
+      }
+    }
+    canvas.ontouchend = function (e) {
+      // 滑动结束
+      painting = false;
+      draw = true;
+    }
+  }
+}
+
+function drawCircle(x, y, radius) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.lineWidth = 4;
+  ctx.lineCap = "round";
+  ctx.lineJoin = "round";
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+  ctx.closePath();
 }
--- js/star.js
+++ js/star.js
@@ -5,61 +5,7 @@ document.body.style.height = windowHeight + "px";
 let tipsHeight = document.getElementById('tips').offsetHeight;
 document.getElementById('tips').style.marginTop = (windowHeight - tipsHeight) / 2 + "px";
 
-let painting = false;
-let draw = false;
-let lastPoint = { x: undefined, y: undefined };
 
-function drawCircle(x, y, radius) {
-    ctx.save();
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2);
-    ctx.fill();
-}
-
-function drawLine(x1, y1, x2, y2) {
-    ctx.lineWidth = 4;
-    ctx.lineCap = "round";
-    ctx.lineJoin = "round";
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-    ctx.closePath();
-}
-
-if (document.body.ontouchstart !== undefined) {
-    // 使用touch事件
-    canvas.ontouchstart = function (e) {
-        // 开始触摸
-        if (!draw) {
-            e.preventDefault();
-            document.getElementById('error').innerText = "";
-            let scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-            let x = e.touches[0].clientX - padLeft;
-            let y = e.touches[0].clientY - (padTop - scrollTop + padHeight * 0.088058);
-            painting = true;
-            lastPoint = { x, y };
-            ctx.save();
-            drawCircle(x, y, 0);
-        }
-    }
-    canvas.ontouchmove = function (e) {
-        // 开始滑动
-        if (painting) {
-            e.preventDefault();
-            let scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-            let x = e.touches[0].clientX - padLeft;
-            let y = e.touches[0].clientY - (padTop - scrollTop + padHeight * 0.088058);
-            let newPoint = { x, y };
-            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
-            lastPoint = newPoint;
-        }
-    }
-    canvas.ontouchend = function (e) {
-        // 滑动结束
-        painting = false;
-        draw = true;
-    }
-}
 
 function closeTips() {
     document.getElementById('overlay').style.display = 'none';
